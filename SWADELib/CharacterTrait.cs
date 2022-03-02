a4623d230ec44c4bcb2af6a38794ac7fec0363d7
Max Courneya <max@courneya.us>
2022-03-01 21:48:42 -0600

    Moved Die rolling from SWADETrait to CharacterTrait class

--- SWADELib/CharacterTrait.cs
+++ SWADELib/CharacterTrait.cs
@@ -19,6 +19,34 @@ namespace SWADELib
             WildDie = wildDie;
         }
 
+        public int[][] RollAsWildCard()
+        {
+            if (TraitDie == null)
+            {
+                throw new SWADEMissingObjectException("Trait Die Object is NULL");
+            }
+
+            if (WildDie == null)
+            {
+                throw new SWADEMissingObjectException("Trait Wild Die Object is NULL");
+            }
+
+            int[][] results = new int[2][];
+            results[0] = TraitDie.RollWithExplosions();
+            results[1] = WildDie.RollWithExplosions();
+            return results;
+        }
+
+        public int[] RollAsExtra()
+        {
+            if (TraitDie == null)
+            {
+                throw new SWADEMissingObjectException("Trait Die Object is NULL");
+            }
+
+            return TraitDie.RollWithExplosions();
+        }
+
         public override string ToString()
         {
             return $"{Trait.Name} [{TraitDie}]";
--- SWADELib/SWADETrait.cs
+++ SWADELib/SWADETrait.cs
@@ -10,35 +10,5 @@ namespace SWADELib
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public Die? Die { get; set; }
-        public Die? WildDie { get; set; }
-
-        public int[][] RollAsWildCard()
-        {
-            if(Die == null)
-            {
-                throw new SWADEMissingObjectException("Trait Die Object is NULL");
-            }
-
-            if(WildDie == null)
-            {
-                throw new SWADEMissingObjectException("Trait Wild Die Object is NULL");
-            }
-
-            int[][] results = new int[2][];
-            results[0] = Die.RollWithExplosions();
-            results[1] = WildDie.RollWithExplosions();
-            return results;
-        }
-
-        public int[] RollAsExtra()
-        {
-            if (Die == null)
-            {
-                throw new SWADEMissingObjectException("Trait Die Object is NULL");
-            }
-
-            return Die.RollWithExplosions();
-        }
     }
 }
